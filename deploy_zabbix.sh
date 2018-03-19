313a38e7779829d0f7f02eac4dd5a0847d9366fc
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-19 14:43:22 +0100

    Updating main script.

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -2,14 +2,13 @@
 SOURCE_DIR=$(dirname $0)
 ZABBIX_DIR=/etc/zabbix
 
-JENKINS_URL=${1:-http://localhost:8080}
-JENKINS_USER=${2}
-JENKINS_PASS=${3}
+SEAFILE_URL=${1:-http://localhost:8000}
+SEAFILE_TOKEN=${2}
 
-mkdir -p ${ZABBIX_DIR}/scripts/agentd/jenkix
-cp ${SOURCE_DIR}/jenkix/jenkix.conf.example ${ZABBIX_DIR}/scripts/agentd/jenkix/jenkix.conf
-cp ${SOURCE_DIR}/jenkix/jenkix.sh ${ZABBIX_DIR}/scripts/agentd/jenkix/
-cp ${SOURCE_DIR}/jenkix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/jenkix.conf
-sed -i "s|JENKINS_URL=.*|JENKINS_URL=\"${JENKINS_URL}\"|g" ${ZABBIX_DIR}/scripts/agentd/jenkix/jenkix.conf
-sed -i "s|JENKINS_USER=.*|JENKINS_USER=\"${JENKINS_USER}\"|g" ${ZABBIX_DIR}/scripts/agentd/jenkix/jenkix.conf
-sed -i "s|JENKINS_PASS=.*|JENKINS_PASS=\"${JENKINS_PASS}\"|g" ${ZABBIX_DIR}/scripts/agentd/jenkix/jenkix.conf
+mkdir -p ${ZABBIX_DIR}/scripts/agentd/seabix
+cp -rv ${SOURCE_DIR}/seabix/seabix.conf.example    ${ZABBIX_DIR}/scripts/agentd/seabix/seabix.conf
+cp -rv ${SOURCE_DIR}/seabix/seabix.sh              ${ZABBIX_DIR}/scripts/agentd/seabix/
+cp -rv ${SOURCE_DIR}/seabix/zabbix_agentd.conf     ${ZABBIX_DIR}/zabbix_agentd.d/seabix.conf
+sed -i "s|SEAFILE_URL=.*|SEAFILE_URL=\"${SEAFILE_URL}\"|g" ${ZABBIX_DIR}/scripts/agentd/seabix/seabix.conf
+sed -i "s|SEAFILE_TOKEN=.*|SEAFILE_TOKEN=\"${SEAFILE_TOKEN}\"|g" ${ZABBIX_DIR}/scripts/agentd/seabix/seabix.conf
+
